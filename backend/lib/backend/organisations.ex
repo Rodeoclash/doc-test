157240fa8abc2c1ee7a8e6d7e9950bb6ceaec4f4
Samuel Richardson <sam@richardson.co.nz>
2026-02-24 21:29:58 +1100

    Add spec to the function

--- backend/lib/backend/organisations.ex
+++ backend/lib/backend/organisations.ex
@@ -5,5 +5,6 @@ defmodule Backend.Organisations do
   @doc """
   Gets a single organisation by id. Returns nil if not found.
   """
+  @spec get(integer()) :: Organisation.t() | nil
   def get(id), do: Repo.get(Organisation, id)
 end
