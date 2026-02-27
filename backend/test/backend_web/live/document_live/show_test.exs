e13ddd92056239573ab4dc1cc7c22172711a7b4b
Samuel Richardson <sam@richardson.co.nz>
2026-02-28 09:36:45 +1100

    Ensure docs page is tested

---
+++ backend/test/backend_web/live/document_live/show_test.exs
@@ -0,0 +1,33 @@
+defmodule BackendWeb.DocumentLive.ShowTest do
+  use BackendWeb.ConnCase
+
+  import Backend.Factory
+  import Phoenix.LiveViewTest
+
+  describe "Show" do
+    test "renders the editor for a valid document", %{conn: conn} do
+      organisation = insert(:organisation)
+      document = insert(:document, organisation: organisation)
+
+      {:ok, _view, html} =
+        live(conn, ~p"/organisations/#{organisation.id}/documents/#{document.id}")
+
+      assert html =~ ~s(id="editor")
+      assert html =~ ~s(data-document-id="#{document.id}")
+    end
+
+    test "raises when document does not exist", %{conn: conn} do
+      organisation = insert(:organisation)
+
+      assert_raise Ecto.NoResultsError, fn ->
+        live(conn, ~p"/organisations/#{organisation.id}/documents/0")
+      end
+    end
+
+    test "raises when organisation does not exist", %{conn: conn} do
+      assert_raise Ecto.NoResultsError, fn ->
+        live(conn, ~p"/organisations/0/documents/0")
+      end
+    end
+  end
+end
