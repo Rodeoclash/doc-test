83a02adbeefcb4fc5c9d160b4c5958d3cfccc12a
Samuel Richardson <sam@richardson.co.nz>
2026-03-08 18:04:57 +1100

    Agents vs user in factories

--- backend/test/support/factory.ex
+++ backend/test/support/factory.ex
@@ -2,6 +2,7 @@ defmodule Backend.Factory do
   @moduledoc false
   use ExMachina.Ecto, repo: Backend.Repo
 
+  alias Backend.Accounts.User
   alias Backend.Documents.Document
   alias Backend.Organisations.Organisation
 
@@ -14,6 +15,22 @@ defmodule Backend.Factory do
   def document_factory do
     %Document{
       name: sequence(:name, &"Document #{&1}"),
+      organisation: build(:organisation)
+    }
+  end
+
+  def user_factory do
+    %User{
+      email: sequence(:email, &"user#{&1}@example.com"),
+      type: :human,
+      organisation: build(:organisation)
+    }
+  end
+
+  def agent_factory do
+    %User{
+      email: sequence(:email, &"agent#{&1}@system.local"),
+      type: :agent,
       organisation: build(:organisation)
     }
   end
