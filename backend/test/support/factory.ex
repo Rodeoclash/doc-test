e58ec1742c665b62a0cebb66b38a5f7093c34eb0
Samuel Richardson <sam@richardson.co.nz>
2026-03-09 17:06:20 +1100

    Update the factory

--- backend/test/support/factory.ex
+++ backend/test/support/factory.ex
@@ -5,6 +5,7 @@ defmodule Backend.Factory do
   alias Backend.Accounts.User
   alias Backend.Documents.Document
   alias Backend.Organisations.Organisation
+  alias Backend.Organisations.OrganisationUser
 
   def organisation_factory do
     %Organisation{
@@ -22,15 +23,20 @@ defmodule Backend.Factory do
   def user_factory do
     %User{
       email: sequence(:email, &"user#{&1}@example.com"),
-      type: :human,
-      organisation: build(:organisation)
+      type: :human
     }
   end
 
   def agent_factory do
     %User{
       email: sequence(:email, &"agent#{&1}@system.local"),
-      type: :agent,
+      type: :agent
+    }
+  end
+
+  def organisation_user_factory do
+    %OrganisationUser{
+      user: build(:user),
       organisation: build(:organisation)
     }
   end
